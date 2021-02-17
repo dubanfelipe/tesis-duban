133a15f3b9dc60147914ee543f0045f15e011c47
Duban_Sanchez <duban.sanchez@usantotomas.edu.co>
2021-02-17 16:41:26 -0500

    put get delete y pos terminado

--- back/controllers/register.controller.js
+++ back/controllers/register.controller.js
@@ -15,19 +15,63 @@ registerCtrl.getRegister = (req, res) => {
     });
 }
 
-registerCtrl.getRegisterById = (req, res) => {    
-    res.json({text: 'buscando usuario'});
+registerCtrl.getRegisterById = (req, res) => {   
+    let id = req.params.id_users;
+    db.query(`SELECT * FROM Users WHERE Id_users='${id}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err });
+            console.log("Hubo un error en la busqueda del Producto" + err);
+        } else {
+            res.json(data);
+        }
+    });
 }
-registerCtrl.createRegister = (req, res) => {
-    //let users = req.body;
-    console.log(req.body); 
-    res.json({text: 'creando usuario'});
+
+registerCtrl.createRegister = async (req, res) => {
+    console.log("usuarios que llego :", req.body);
+    usuarios = req.body;
+    console.log(usuarios);
+    var query = `INSERT INTO users (cedula,Nombre,Codigo,Facultad,Semestre,Ocupacion)
+    VALUES ('${usuarios.Cedula}','${usuarios.Nombre}','${usuarios.Codigo}','${usuarios.Facultad}','${usuarios.Semestre}','${usuarios.Ocupacion}')`;
+    db.query(query, function(err, data) {
+        if (err) {
+            res.json({ error: err });
+            console.log("Hubo un error INSERTANDO USUARIO" + err);
+        } else {
+            res.json(data);
+        }
+    });
 }
+
 registerCtrl.deleteRegisterById = (req, res) =>{
-    res.json({text: 'eliminando usuario'});
+    let id = req.params.id_users;
+    db.query(`DELETE FROM Users WHERE Id_users= '${id}'`, (err, data) => {
+        if (err) {
+            res.json({ error: err });
+            console.log("Hubo un error ELIMINANDO USUARIO" + err);
+        } else {
+            res.json({message: 'Usuario eliminado'});
+        }
+    });
 }
 registerCtrl.updateRegisterById = (req, res) =>{
-    res.json({text: 'editando usuario'});
+    const Id_update = req.params.id_users;
+    const update = req.body;
+    console.log(`dato 1 ${Id_update} id usuarios ${JSON.stringify(req.body)}`);
+    var query = `UPDATE Users SET Cedula = '${update.Cedula}', Nombre ='${update.Nombre}', Codigo ='${update.Codigo}', Facultad ='${update.Facultad}', Semestre ='${update.Semestre}', Ocupacion ='${update.Ocupacion}'  WHERE Id_users = '${Id_update}'`;
+    try {
+        db.query(query, (err, data) => {
+            if (err) {
+                res.json({ error: err });
+                console.log("Hubo un error actualizando el usuario" + err);
+            } else {
+                res.json(data);
+            }
+        });
+    } catch (E) {
+        console.log('error', E);
+        res.json({message: 'error en el servidor actualizando la convocatoria' })
+    }
 }
 
 module.exports = registerCtrl;
